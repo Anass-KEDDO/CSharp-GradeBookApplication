ec97f97b693a0cd71264a84ff2a8c3b97fc11236
Anass-KEDDO <anass.keddo@capgemini.com>
2021-05-29 19:25:49 +0100

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,22 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        override public char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            if (averageGrade > 80)
+                return 'A';
+            if (averageGrade > 60)
+                return 'B';
+            if (averageGrade > 40)
+                return 'C';
+            if (averageGrade > 20)
+                return 'D';
+
+            return 'F';
+        }
     }
 }
